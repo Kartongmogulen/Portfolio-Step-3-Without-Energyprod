8c58b7296a0be016b6aaa0c5bd82c6e7da60ea9b
David Jakobsson <davidjakobsson89@gmail.com>
2021-10-27 18:23:11 +0200

    Create playerStockPortfolio.cs

---
+++ Portfolio Step 3.1.1 Stocks/Assets/Scripts/Buttons/playerStockPortfolio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerStockPortfolio : MonoBehaviour
+{
+	public GameObject portfolioPanelGO;
+	public GameObject sectorInfoPanelGO;
+
+	private int portfolioPanelStatus;
+
+
+	public void activatePortPanel(){
+
+		if (portfolioPanelStatus == 0){
+
+		portfolioPanelGO.SetActive (true);
+		sectorInfoPanelGO.SetActive (false);
+		portfolioPanelStatus = 1;
+		}
+
+		else {
+			portfolioPanelGO.SetActive (false);
+			//sectorInfoPanelGO.SetActive (false);
+			portfolioPanelStatus = 0;
+
+		}
+	}
+}
