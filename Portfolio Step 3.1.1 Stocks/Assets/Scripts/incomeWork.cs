85744f149c61da0887f45f097d1ce8a19a1ee915
David Jakobsson <davidjakobsson89@gmail.com>
2021-10-27 18:24:01 +0200

    Create incomeWork.cs

---
+++ Portfolio Step 3.1.1 Stocks/Assets/Scripts/incomeWork.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class incomeWork : MonoBehaviour
+{
+	//Script med data kring inkomst från jobb som erhålls då varje runda är slut
+
+	public float workIncomeStart;
+	public float workIncomeNow;
+	public Text workIncomeText;
+
+	public float incomeWorkLife; //Inkomst från arbete
+	//public Text incomeWorkLifeText;
+
+	public GameObject MoneyPanel;
+	public int infoMoneyOnOrOff = 0;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		workIncomeNow = workIncomeStart;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		workIncomeText.text = "Work/month: " + workIncomeNow;  
+	}
+
+	public void showMoneyPanel(){
+
+
+
+		if (infoMoneyOnOrOff == 0){
+			MoneyPanel.SetActive (true);
+			infoMoneyOnOrOff++;
+		}
+
+		else {
+			MoneyPanel.SetActive (false);
+			infoMoneyOnOrOff--;
+		}
+
+	}
+
+	public void incomeLifeFromWork()
+
+	{
+		incomeWorkLife = incomeWorkLife + workIncomeNow;
+		//incomeWorkLifeText.text = "Income from Work: " + incomeWorkLife;
+	}
+}
